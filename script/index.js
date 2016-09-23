b91862761c606bbcb7079e8ccd194d3d5e8b5e35
Andriy Sikora <Andriy.Sikora@gmail.com>
2016-09-24 00:32:18 +0300

    Add render function

--- script/index.js
+++ script/index.js
@@ -1,33 +1,39 @@
-window.onload = function(){
-    var ul = document.createElement('ul');
-    ul.setAttribute('class', 'list-group');
-    document.body.appendChild(ul);
+window.onload = function() {
+    var app = {
+        render: function () {
+            var ul = document.createElement('ul');
+            ul.setAttribute('class', 'list-group');
+            document.body.appendChild(ul);
 
-    var docFrag = document.createDocumentFragment();
-        var tasks = [
-            {
-                value: "Buy milk"
-            },
-            {
-                value: "Go to the gym"
-            },
-            {
-                value: "Play tennis"
-            },
-            {
-                value: "Drink beer"
-            },
-            {
-                value: "Read the book"
+            var docFrag = document.createDocumentFragment();
+                     var tasks = [
+                        {
+                        value: "Buy milk"
+                        },
+                        {
+                        value: "Go to the gym"
+                        },
+                        {
+                        value: "Play tennis"
+                        },
+                        {
+                        value: "Drink beer"
+                        },
+                        {
+                        value: "Read the book"
+                        }
+                    ];
+
+            for (var i=0; i < tasks.length; i++) {
+                var li = document.createElement('li');
+                li.setAttribute('class', 'list-group-item');
+                li.innerHTML = tasks[i].value;
+                docFrag.appendChild(li);
             }
-        ];
-
-    for (var i=0; i < tasks.length; i++) {
-        var li = document.createElement('li');
-        li.setAttribute('class', 'list-group-item');
-        li.innerHTML = tasks[i].value;
-        docFrag.appendChild(li);
+            ul.appendChild(docFrag);
+        }
     }
-    ul.appendChild(docFrag);
-
+    app.render();
 };
+
+
