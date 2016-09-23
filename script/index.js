34ac5d6d0d2460309f33d87e8708ad61ef30ef7b
Andriy Sikora <Andriy.Sikora@gmail.com>
2016-09-24 01:05:30 +0300

    Add array to the app

--- script/index.js
+++ script/index.js
@@ -1,39 +1,38 @@
 window.onload = function() {
     var app = {
+       tasks : [
+                {
+                value: "Buy milk"
+                },
+                {
+                value: "Go to the gym"
+                },
+                {
+                value: "Play tennis"
+                },
+                {
+                value: "Drink beer"
+                },
+                {
+                value: "Read the book"
+                }
+        ],
         render: function () {
             var ul = document.createElement('ul');
             ul.setAttribute('class', 'list-group');
             document.body.appendChild(ul);
 
             var docFrag = document.createDocumentFragment();
-                     var tasks = [
-                        {
-                        value: "Buy milk"
-                        },
-                        {
-                        value: "Go to the gym"
-                        },
-                        {
-                        value: "Play tennis"
-                        },
-                        {
-                        value: "Drink beer"
-                        },
-                        {
-                        value: "Read the book"
-                        }
-                    ];
-
-            for (var i=0; i < tasks.length; i++) {
+            for (var i=0; i < this.tasks.length; i++) {
                 var li = document.createElement('li');
                 li.setAttribute('class', 'list-group-item');
-                li.innerHTML = tasks[i].value;
+                li.innerHTML = this.tasks[i].value;
                 docFrag.appendChild(li);
             }
             ul.appendChild(docFrag);
         }
     }
-    app.render();
+    app.render(this);
 };
 
 
